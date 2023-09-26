6605c94ea36769bea1ae13936ccec034a59c49ab
Joseph Chingalo <profschingalo@gmail.com>
2023-09-26 19:23:11 +0300

    feat: 🎸 support for get program metdata

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,5 @@ export * from './excel-util';
 export * from './file-util';
 export * from './http-util';
 export * from './logs-util';
+export * from './dhis2-option-set-util';
+export * from './dhis2-program-util';
