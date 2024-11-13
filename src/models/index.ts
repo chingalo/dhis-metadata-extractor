93b45bfc798b228a090307c64a66b451d8ce80e4
Joseph Chingalo <profschingalo@gmail.com>
2024-11-13 13:28:13 +0300

    chore: model transformer for program rule actions

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,3 +3,4 @@ export * from './dhis2-option-set';
 export * from './dhis2-program';
 export * from './dhis2-program-rule';
 export * from './dhis2-program-rule-variable';
+export * from './dhis2-program-rule-action';
